1360c9158318aaa50f5278e3c0029bbe5f081d1c
Ungewitter_Cyrus <ungec064@hsd.k12.or.us>
2019-10-24 12:57:42 -0700

    adding all initial file including .gitignore

---
+++ Assets/Player 1 scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 4.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.None))
+        {
+            rb.velocity = transform.right * 0;
+        }
+    }
+}
